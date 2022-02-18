3af1582bf15a4844c10bb9d6e63de5e4035a5f01
lijian <ljhhh@qq.com>
2022-02-18 22:02:31 +0800

    1.完成了首页的底部布局 2.使用了BEM（block element modifier）的css命名规范

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,54 @@
 <template>
-  <div id="nav">
-      123
-  </div>
+    <div class="docker" id="nav">
+        <div class="docker__item docker__item--active">
+            <div class="iconfont">&#xe600;</div>
+            <div class="docker__title">首页</div>
+        </div>
+        <div class="docker__item">
+            <div class="iconfont">&#xe601;</div>
+            <div class="docker__title">购物车</div>
+        </div>
+        <div class="docker__item">
+            <div class="iconfont">&#xe602;</div>
+            <div class="docker__title">订单</div>
+        </div>
+        <div class="docker__item">
+            <div class="iconfont">&#xe7c7;</div>
+            <div class="docker__title">我的</div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss">
+.docker {
+    box-sizing: border-box;
+    position: absolute;
+    display: flex;
+    padding: 0 .18rem;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 0.49rem;
+    border-top: 1px solid #f1f1f1;
+
+    &__item {
+        flex: 1;
+        text-align: center;
+
+        .iconfont {
+            margin: .07rem 0 .02rem 0;
+            font-size: .18rem;
+        }
+
+        &--active {
+            color: rebeccapurple;
+        }
+    }
+
+    &__title {
+        font-size: 20px;
+        transform: scale(.5, .5);
+        transform-origin: center top;
+    }
+}
 </style>
